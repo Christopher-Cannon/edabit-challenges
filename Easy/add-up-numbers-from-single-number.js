3de8033488fcfa43abd2fec1dd5bc705e5da5ff9
Christopher-Cannon <cscannon92@yahoo.com>
2021-12-02 15:26:54 +0000

    Add left-shift-by-powers-of-two solution

--- Easy/add-up-numbers-from-single-number.js
+++ Easy/add-up-numbers-from-single-number.js
@@ -4,23 +4,27 @@
 function addUp(n) {
   let number = typeof n === "string" ? Number(n.trim()) : n;
 
-  if (number > 0 && number < 1001 && !Number.isNaN(number)) {
+  if (!Number.isNaN(number) && number > 0 && number < 1001) {
     let result = 0;
 
-    for (let i = number; i > 0; i--) {
-      result += i;
-    }
+    for (let i = number; i > 0; i--) result += i;
+
     return result;
   }
   return 0;
 }
 
+// Expected
 console.log( addUp(4) );
 console.log( addUp(13) );
 console.log( addUp(600) );
 
+// Extreme
+console.log( addUp(" 4 ") );
+
+// Exceptional
 console.log( addUp(-1) );
 console.log( addUp(1200) );
 console.log( addUp("Hello, World") );
-
-console.log( addUp(" 4 ") );
+console.log( addUp("") );
+console.log( addUp(" ") );
---
+++ Easy/left-shift-by-powers-of-two.js
@@ -0,0 +1,24 @@
+// Left Shift by Powers of Two
+// https://edabit.com/challenge/pB6CF3rFBi8ykJ3Br
+
+function shiftToLeft(x, y) {
+  let n1 = typeof x === "string" ? Number(x.trim()) : x;
+  let n2 = typeof y === "string" ? Number(y.trim()) : y;
+
+  if (Number.isNaN(n1) || Number.isNaN(n2)) return 0;
+
+  return x * 2 ** Math.abs(y);
+}
+
+// Expected
+console.log( shiftToLeft(5, -2) );
+console.log( shiftToLeft(10, 3) );
+console.log( shiftToLeft(-6, 5) );
+console.log( shiftToLeft(12, 4) );
+
+// Extreme
+console.log( shiftToLeft("12", " 4 ") );
+
+// Exceptional
+console.log(shiftToLeft("Hello", "World"));
+
